dfda050b02442cc1f3ac2400c9125f8a5183b4d3
Juan Carlos Medina <juanca@mavenlink.com>
2021-04-10 12:45:21 -0700

    feat(query): implement attributes matching
    The error messaging isn't so great but that can be improved over time.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const screen = require('@testing-library/dom');
+const {
+  screen,
+  waitFor,
+} = require('@testing-library/dom');
 
-async function accessibleQuery(label) {
-  const node = await screen.findByLabelText(document.body, label);
+async function accessibleQuery(label, attributes = {}) {
+  const node = await waitFor(() => {
+    const node = screen.getByLabelText(label);
+
+    const matches = Object.entries(attributes).every(([attribute, value]) => {
+      return node.getAttribute(attribute) === value;
+    });
+
+    if (matches) {
+      return node;
+    } else {
+      throw Error(`Element (${label}) not found with specific attributes.`);
+    }
+  });
+
 
   return node;
 }
--- index.test.js
+++ index.test.js
@@ -1,10 +1,27 @@
 const query = require('./index.js');
 
 it('finds labelled elements', async () => {
+  setTimeout(() => {
+    document.body.innerHTML =
+      '<label for="test-input">Test label</label>' +
+      '<input id="test-input" />' +
+      '';
+  }, 10);
+
+  expect(await query('Test label')).toBe(document.getElementById('test-input'));
+});
+
+it('finds disabled elements', async () => {
   document.body.innerHTML =
     '<label for="test-input">Test label</label>' +
     '<input id="test-input" />' +
     '';
 
-  expect(await query('Test label')).toBe(document.getElementById('test-input'));
-})
+  setTimeout(() => {
+    document.getElementById('test-input').setAttribute('disabled', '');
+  }, 10);
+
+  const node = await query('Test label', { disabled: '' });
+  expect(node).toBe(document.getElementById('test-input'));
+  expect(node.getAttribute('disabled')).toBe('');
+});
